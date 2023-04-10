8cccf62dfef9fcf9d2d9221ad6b8e2409d710d3f
Shashank Mistry <shashankmistry30@gmail.com>
2023-04-10 13:19:01 -0400

    product schema created

---
+++ models/product.js
@@ -0,0 +1,29 @@
+//created model for product with fields name, description, details, image, price
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
